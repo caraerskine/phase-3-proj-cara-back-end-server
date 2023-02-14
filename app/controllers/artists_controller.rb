5768103e78002998c2c6d9a0b976304172f53ac0
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-02-14 13:28:01 -0500

    took out notes

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,23 +1,16 @@
 class ArtistsController < ApplicationController
 
-#this is to get the individual artists paintings to show up when user clicks "see all paintings by this artist" button
-  
-#GET
-#gets individual artists paintings
+
 get '/artists/:id/paintings' do
     paintings = Artist.find_by(id: params[:id])
     paintings.to_json(include: :paintings)
   end
 
-  #GET
-#gets all the artists
   get '/artists' do
     artists = Artist.all
     artists.to_json(include: :paintings)
   end
 
-  #CREATE
-  #add artist
   post '/artists' do
     artists = Artist.create(params)
     artists.to_json
@@ -25,8 +18,7 @@ get '/artists/:id/paintings' do
 
 end
 
-#should above be
-#'/artists/:id/artists/new'   idk
+
 
 
 
--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -1,30 +1,21 @@
 class PaintingsController < ApplicationController
  
-#READ
-#get all the paintings
+
  get '/paintings' do
-    # pry
     paintings = Painting.all
     paintings.to_json(include: :artist)
   end
 
-# CREATE
-# add painting
 post '/paintings' do
-    # binding.pry
     painting = Painting.create(params)
     painting.to_json
   end
 
-#DELETE
-#delete specific painting
   delete '/paintings/:id' do 
     painting = Painting.find_by(id: params[:id])
     painting.destroy
   end
 
-#UPDATE
- #update the painting with the LIKER button on painting card
   patch '/paintings/:id' do
     painting = Painting.find_by(id: params[:id])
     painting.update(like: params[:like])
